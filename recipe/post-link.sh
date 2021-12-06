de64b4f030009c8e952a8a01569f1c0449483597
fernandez <mathieu.fernandez@e-i.com>
2021-12-06 13:19:44 +0100

    get repo for lg, trf

--- recipe/post-link.sh
+++ recipe/post-link.sh
@@ -2,8 +2,8 @@
 # We redirect stderr & stdout to conda's .messages.txt; for details, see
 #     http://conda.pydata.org/docs/building/build-scripts.html
 {
-  cmd="\"${PREFIX}/bin/spacy\" link ${PKG_NAME/spacy-model-/} ${PKG_NAME/spacy-model-/}"
-  if [ -x \"${PREFIX}/bin/spacy\" ]; then 
+  cmd="\"${PREFIX}/bin/spacy\" link ${PKG_NAME/spacy-model-/} ${PKG_NAME/spacy-model-/} ${PKG_NAME/spacy-model-/} ${PKG_NAME/spacy-model-/}"
+  if [ -x \"${PREFIX}/bin/spacy\" ]; then
     $cmd
   else
     echo "You can link this model to spacy by running:"
